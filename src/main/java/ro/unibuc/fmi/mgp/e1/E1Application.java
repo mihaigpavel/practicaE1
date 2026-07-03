3d87da21ce18b51d43e28b1797fa9e55ded1a8e3
Pavel Mihai George <mihai.gpavel@gmail.com>
2026-07-03 14:05:52 +0300

    generare initiala proiect cu Spring Initializer

---
+++ src/main/java/ro/unibuc/fmi/mgp/e1/E1Application.java
@@ -0,0 +1,13 @@
+package ro.unibuc.fmi.mgp.e1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E1Application {
+
+	static void main(String[] args) {
+		SpringApplication.run(E1Application.class, args);
+	}
+
+}
