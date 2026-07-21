9a697de81b2273cc7ba0d7e09429c6bdee6f24a3
Pavel Mihai George <mihai.gpavel@gmail.com>
2026-07-21 17:10:27 +0300

    eliminare informatii PK din PositionRequest

--- src/main/java/ro/unibuc/fmi/mgp/e1/position/PositionRequest.java
+++ src/main/java/ro/unibuc/fmi/mgp/e1/position/PositionRequest.java
@@ -4,18 +4,10 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public class PositionRequest {
-    private Long id;
     @NotBlank
     @Size(min = 1, max = 255)
     private String name;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
@@ -28,7 +20,6 @@ public class PositionRequest {
     @Override
     public String toString() {
         return "PositionRequest{" +
-                "id=" + id +
                 ", name='" + name + '\'' +
                 '}';
     }
